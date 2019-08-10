e6e6662503d5dc34c601ba8d4496545a025baf43
Huang ZH <13193697081@163.com>
2019-08-10 14:25:04 +0800

    update  index.html  over  1.0

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -9,40 +9,198 @@
         </h3>
 
         <div class="box-items">
-          
-            <img src="img/index/gscs-1.png" />
-          
+          <img src="img/index/gscs-1.png" />
+
           <div class="items">
             <div class="item">
               <img src="img/index/gscs-2.png" />
-              <p>
-                定制财税及管理咨询及税收筹划方案策划,出具专项财税审计年报审计
-              </p>
+              <p>定制财税及管理咨询及税收筹划方案策划,出具专项财税审计年报审计。</p>
               <router-link to>财税咨询</router-link>
+            </div>
+
+            <div class="item">
+              <img src="img/index/gscs-3.png" />
+              <p>帮助企业维护税务关系,降低企业在经营过程中的财务成本,全力发展核心业务。</p>
+              <router-link to>代理记账</router-link>
             </div>
             <div class="item">
-              <img src="img/index/gscs-2.png" />
-              <p>
-                定制财税及管理咨询及税收筹划方案策划,出具专项财税审计年报审计
-              </p>
-              <router-link to>财税咨询</router-link>
+              <img src="img/index/gscs-4.png" />
+              <p>各大类型公司注册、变更。</p>
+              <router-link to>代理登记</router-link>
             </div>
             <div class="item">
-              <img src="img/index/gscs-2.png" />
-              <p>
-                定制财税及管理咨询及税收筹划方案策划,出具专项财税审计年报审计
-              </p>
-              <router-link to>财税咨询</router-link>
+              <img src="img/index/gscs-5.png" />
+              <p>卫生许可证,食品许可证,餐饮服务许可证等。</p>
+              <router-link to>许可证办理</router-link>
             </div>
-            <div class="item">
-              <img src="img/index/gscs-2.png" />
-              <p>
-                定制财税及管理咨询及税收筹划方案策划,出具专项财税审计年报审计
-              </p>
-              <router-link to>财税咨询</router-link>
-            </div>
-            
           </div>
+        </div>
+      </div>
+      <!-- 区域二 -->
+      <div class="chanquan">
+        <div class="cq-left">
+          <h3>
+            <span class="iconfont icon-r"></span>知识产权
+            <router-link to>点击了解更多&gt;&gt;</router-link>
+          </h3>
+          <div class="items">
+            <div class="item">
+              <h3>商标注册</h3>
+              <div></div>
+              <div class="every">
+                <p>
+                  <span>普通商标注册</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+                <p>
+                  <span>国内商标注册</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+                <p>
+                  <span>商标无效宣告申请</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <h3>版权登记</h3>
+              <div></div>
+              <div class="every">
+                <p>
+                  <span>版权服务</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+                <p>
+                  <span>企业版权</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+                <p>
+                  <span>版权登记</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <h3>专利申请</h3>
+              <div></div>
+              <div class="every">
+                <p>
+                  <span>发明专利申请</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+                <p>
+                  <span>实用新型专利申请</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+                <p>
+                  <span>外观设计专利申请</span>
+                  <router-link to>点击申请</router-link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="c-right">
+          <h3>商标注册变更</h3>
+          <router-link to>
+            <img src="img/index/gscs-6.png" />
+          </router-link>
+        </div>
+      </div>
+      <!-- 三 -->
+      <div class="box">
+        <h3>
+          <span class="iconfont icon-rongzifuwu"></span>融资咨询
+        </h3>
+
+        <div class="box-items">
+          <img src="img/index/rz-1.png" />
+
+          <div class="items">
+            <div class="item">
+              <img src="img/index/rz-2.png" />
+              <p>过桥资金是一种短期资金的通,期限以六个月为限,是一种与长期资金相对接的资金。提供过桥资金的目的是通过过桥资金的融通,达到与长期。</p>
+              <router-link to>企业过桥</router-link>
+            </div>
+
+            <div class="item">
+              <img src="img/index/rz-3.png" />
+              <p>为企业财务管理优化和人员能力提提供包括架构重组、流程梳理、战略承接、发展规划、制度设计、人员若项培训在内的系统解决方案。</p>
+              <router-link to>短期周转</router-link>
+            </div>
+            <div class="item">
+              <img src="img/index/rz-4.png" />
+              <p>为企业财务管理优化和人员能力提升提供包括架构重组、流程梳理、歧略承接、发展规划、制度设计、人员专顶培训在内的系统解决方案。</p>
+              <router-link to>承兑贴现</router-link>
+            </div>
+            <div class="item">
+              <img src="img/index/rz-5.png" />
+              <p>为企业财务管理优化和人员能力提升提供包括架构重组、流程梳理、战略承接、发展规划、制度设计、人员专项培训在内的系统解决方案。</p>
+              <router-link to>担保保证</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 四 -->
+      <div class="box">
+        <h3>
+          <span class="iconfont icon-zichan"></span>资产管理
+        </h3>
+
+        <div class="box-items">
+          <img src="img/index/zc-1.png" />
+
+          <div class="items">
+            <div class="item">
+              <img src="img/index/zc-2.png" />
+              <p>为企业财务管理优化和人员能力提升提供包括架构重组、流程梳理、战略承接、发展规划、制度设计、人员专培训在内的系统解決方案。</p>
+              <router-link to>不良资产处理</router-link>
+            </div>
+
+            <div class="item">
+              <img src="img/index/zc-3.png" />
+              <p>为企业财务管理优化和人员能力提升提供包括架构重组、程梳理、战略承接、发展规划、制度设计、人员专项培训在内的系统解決方案。</p>
+              <router-link to>股权投资</router-link>
+            </div>
+            <div class="item">
+              <img src="img/index/zc-4.png" />
+              <p>为企业财务管理优化和人员能力提升提供包括架构重组、流程梳理、战略承接、发展规划、制度设计、人员专项培训在内的系统解决方案。</p>
+              <router-link to>资产评估</router-link>
+            </div>
+            <div class="item">
+              <img src="img/index/zc-5.png" />
+              <p>为企业财务管理优化和人员能力提升提供包括架构重组、程梳理、战略承接、发展规划、制度设计、人员专项培训在内的系统解决方案。</p>
+              <router-link to>债权转让</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fuwu">
+        <h2>提供专业的企业服务</h2>
+        <div>
+          <img src="img/index/3g-1.png" />
+        </div>
+        <p>企欣查为这些企业提供多领域的服务外包,以专业服务弥补企业发展短板,帮助企业专注于核心业务在自己的专业领域提升竞争力。世界专业而美好,爱企网则致力于用服务让企业更专业。</p>
+        <p>专业的人做专业的事”这条现代社会的基本原则在中小微创企业中却显得尤为奢侈。从内到外的各种复杂业务,让无数企业为之分心。爱企网致力于为中小微创企业提供专业、全面的企业服务型产品,让企业只需专注自身的核心领域,提升"专注度"与竞争カ",轻兵上阵领先他人。</p>
+        <img src="img/index/fw-1.png" id="t1" />
+        <img src="img/index/fw-2.png" id="t2" />
+        <img src="img/index/fw-3.png" id="t3" />
+        <img src="img/index/fw-1.png" id="b1" />
+        <img src="img/index/fw-2.png" id="b2" />
+        <img src="img/index/fw-3.png" id="b3" />
+      </div>
+      <!-- 用户合作 -->
+      <div class="hezuo">
+        <h3>合作客户</h3>
+        <div>
+          <img src="img/index/01.png" />
+          <img src="img/index/02.png" />
+          <img src="img/index/03.png" />
+          <img src="img/index/04.png" />
+          <img src="img/index/05.png" />
+          <img src="img/index/06.png" />
+          <img src="img/index/07.png" />
+          <img src="img/index/08.png" />
         </div>
       </div>
     </div>
